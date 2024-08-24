b1aac61bdae2a59e6014a11c7a47492f65748870
samya-roy <samyaroy999@gmail.com>
2024-08-24 20:49:10 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Navbar from './Components/Navbar/NAvbar'
+
+
+
+function App() {
+
+  return (
+    <div className='mx-auto max-w-screen-lg'>
+      <Navbar />
+      <div className=''>
+        <img src="https://i.ibb.co/f25ykWB/16768819-tp231-presentation-04-383.jpg" alt="" />
+      </div>
+    </div>
+  )
+}
+
+export default App
